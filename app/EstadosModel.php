dc0db0399987d51c7ab27f8ecee67f8b62978eec
Saira Martinez <al221910498@gmail.com>
2021-03-01 11:15:34 -0600

    Modelos

---
+++ app/EstadosModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EstadosModel extends Model
+{
+    //
+    protected $table = 'tb_estados';
+    protected $primaryKey = 'id_estado';
+    protected $fillable = ['nombre'];
+}
---
+++ app/MunicipiosModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MunicipiosModel extends Model
+{
+    //
+    protected $table = 'tb_municipios';
+    protected $primaryKey = 'id_municipio';
+    protected $fillable = ['nombre','id_estado'];
+
+}
